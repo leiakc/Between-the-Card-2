9e2832955f59c2f30592484552958777072435ee
leiakc <57116108+leiakc@users.noreply.github.com>
2020-11-03 02:58:16 -0500

    Fungus - Dialogue & Trigger Dialogue
    - added Fungus asset to project
    - made a basic flowchart for the Fool's dialogue / story
    - made template flowchart for Tower's dialogue / story
    - added "is trigger" collider around Fool/Tower character
    - created script: when player touches collider of Fool/Tower character, dialogue starts
    //troubleshooted how to get touch trigger to work

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public BlockReference blockRef;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        blockRef.Execute();
+    }
+}
