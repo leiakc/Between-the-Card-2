59f288201c51de54efe168a455e87688e81382fa
leiakc <57116108+leiakc@users.noreply.github.com>
2020-11-16 17:48:27 -0500

    Modified Dialogue Trigger & Changed Fool Music
    - dialogue box now launches when player is in the trigger collider & clicks the mouse button
    note: if you end the dialogue, you need to leave the collider and re-enter it to launch it again
    possible moving forward: add menu after each dialogue's ending OR change code again
    
    - changed fool music to a different song

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -6,20 +6,74 @@ using Fungus;
 public class DialogueTrigger : MonoBehaviour
 {
     public BlockReference blockRef;
+
+    public bool mousePressed;
+
+    public bool inArea;
+
+    public bool blockExecuted = false;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (Input.GetMouseButtonDown(0) && blockExecuted == false)
+        {
+            blockRef.Execute();
+            blockExecuted = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        blockExecuted = false;
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        *//*if (Input.GetMouseButtonDown(0))
+        {
+            mousePressed = true;
+        } else
+        {
+            mousePressed = false;
+        }*//*
+
+        if(inArea == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                blockRef.Execute();
+                blockExecuted = true;
+            }
+        }
+           
     }
 
     void OnTriggerEnter(Collider other)
     {
-        blockRef.Execute();
+        *//*if (mousePressed)
+        {
+            blockRef.Execute();
+        }*//*
+        if(blockExecuted == false)
+        {
+            inArea = true;
+        } else if (blockExecuted == true)
+        {
+            inArea = false;
+        }
+        
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inArea = false;
+        blockExecuted = false;
+    }*/
 }
