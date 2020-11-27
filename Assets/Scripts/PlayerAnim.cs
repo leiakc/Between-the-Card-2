d899bbf64e7af87df670be259d1322a7ebe6c101
mcshanneng <43505218+mcshanneng@users.noreply.github.com>
2020-11-27 03:03:32 -0500

    Shannen's Late night additions :)
    -increased scene size
    -Added URP things to allow shaders to work
    -Added Toon Shader to character
    -Added animations to the character's varios states

---
+++ Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w") || Input.GetKey("s") || Input.GetKey("a") || Input.GetKey("d"))
+        {
+            anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
+    }
+}
