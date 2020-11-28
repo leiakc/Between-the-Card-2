62636049b4be5b01a2d134eaf17f8627b7609bda
leiakc <57116108+leiakc@users.noreply.github.com>
2020-11-27 22:01:04 -0500

    Dialogue Changes & Input Arrow Keys Animation
    Dialogue Changes
    - fixed bug where it would play multiple dialogue w one click
    --- ditto for menu options
    - added "filler" dialogue (ex. crew before get mission from fool)
    - created placeholder & trigger colliders for fool, crew, shell
    - destroy shell when picked up
    - shell storyline comes up first w fool after shell is picked up (but not before intro dialogue)
    - playtested so everything works
    
    Input Arrow Keys Animation
    - noticed aswd played animation, but not arrow keys
    --> so added the arrow keys to the animation play script

--- Assets/Scripts/MasterDialogueCode.cs
+++ Assets/Scripts/MasterDialogueCode.cs
@@ -7,118 +7,136 @@ public class MasterDialogueCode : MonoBehaviour
 {
     /*public bool pause = false;*/
 
+    public Flowchart allVarFlowchart;
+
     public BlockReference blockRef1;
-    public bool block1Executed = false;
+    bool block1Executed;
 
     public BlockReference blockRef2;
-    public bool block2Executed = false;
+    bool block2Executed;
 
     public BlockReference blockRef3;
-    public bool block3Executed = false;
+    bool block3Executed;
 
     public BlockReference blockRef4;
-    public bool block4Executed = false;
+    bool block4Executed;
 
     public BlockReference blockRef4_25;
 
     public BlockReference blockRef4_5;
-    public bool blockCrewmateExecuted = false;
+    bool blockCrewmateExecuted;
 
     public BlockReference blockRef4_5_5;
 
     public BlockReference blockRef4_75;
-    public bool blockDoneShipExecuted = false;
+    bool blockDoneShipExecuted;
 
     public BlockReference blockRef5_0;
 
     public BlockReference blockRef5;
-    public bool block5Executed = false;
-    public bool haveSeashell = false;
+    bool block5Executed;
+    bool haveSeashell;
+    private GameObject shell;
 
     public BlockReference blockRef6;
+
+    bool inOptionDia;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        shell = GameObject.FindGameObjectWithTag("Shell");
     }
 
     // Update is called once per frame
     void Update()
     {
+        block1Executed = allVarFlowchart.GetBooleanVariable("done1");
+        block2Executed = allVarFlowchart.GetBooleanVariable("done2");
+        block3Executed = allVarFlowchart.GetBooleanVariable("done3");
+        block4Executed = allVarFlowchart.GetBooleanVariable("done4");
+        blockCrewmateExecuted = allVarFlowchart.GetBooleanVariable("doneCrew");
+        blockDoneShipExecuted = allVarFlowchart.GetBooleanVariable("doneShip");
+        haveSeashell = allVarFlowchart.GetBooleanVariable("haveShell");
+        block5Executed = allVarFlowchart.GetBooleanVariable("done5");
+        inOptionDia = allVarFlowchart.GetBooleanVariable("inOptionDia");
 
+        if(haveSeashell == true)
+        {
+            Object.Destroy(shell);
+        }
     }
     private void OnTriggerStay(Collider other) //later: fool / dif one for crewmate, dif one for seashell
     {
+        
         if (Input.GetMouseButtonDown(0))
         {
-            if (block1Executed == false)
+            if (other.tag == "Fool" && inOptionDia == false)
             {
-                blockRef1.Execute();
-                block1Executed = true;
-            }
-            else if (block2Executed == false)
-            {
-                blockRef2.Execute();
-                block2Executed = true;
-            }
-            else if (block3Executed == false)
-            {
-                blockRef3.Execute();
-                block3Executed = true;
-            }
-            else if (block4Executed == false)
-            {
-                blockRef4.Execute();
-                block4Executed = true;
-            }
-            else if (blockCrewmateExecuted == false)
-            {
-                blockRef4_25.Execute();
-            }
-            else if (blockDoneShipExecuted == false)
-            {
-                blockRef4_75.Execute();
-                blockDoneShipExecuted = true;
-            }
-            else
-            {
-                blockRef6.Execute();
-            }
+                if (block1Executed == false)
+                {
+                    blockRef1.Execute();
 
-            //seashell storyline
-            if (haveSeashell == true && block5Executed == false)
-            {
-                blockRef5.Execute();
-                block5Executed = true;
+                }
+                else if (haveSeashell == true && block5Executed == false)
+                {
+                    //seashell storyline
+                    blockRef5.Execute();
+
+                }
+                else if (block2Executed == false)
+                {
+                    blockRef2.Execute();
+
+                }
+                else if (block3Executed == false)
+                {
+                    blockRef3.Execute();
+
+                }
+                else if (block4Executed == false)
+                {
+                    blockRef4.Execute();
+
+                }
+                else if (blockCrewmateExecuted == false)
+                {
+                    blockRef4_25.Execute();
+                }
+                else if (blockDoneShipExecuted == false)
+                {
+                    blockRef4_75.Execute();
+
+                }
+                else
+                {
+                    blockRef6.Execute();
+                }
+
+
+
             }
-        }
+            if (other.tag == "Crew")
+            {
+                if (Input.GetMouseButtonDown(0) && blockCrewmateExecuted == false)
+                {
+                    blockRef4_5.Execute();
+
+                }
+                else if (blockCrewmateExecuted == true)
+                {
+                    blockRef4_5_5.Execute();
+                }
+            }
+            if (other.tag == "Shell" && haveSeashell == false && inOptionDia == false)
+            {
+                blockRef5_0.Execute();
+            }
+        } // mouse clicked end 
 
     }
 
-    //crewmate dialogue
-    /*private void OnTriggerStay(Collider other) //crewmate
-    {
-        if (Input.GetMouseButtonDown(0) && blockCrewmateExecuted == false)
-        {
-            blockRef4_5.Execute();
-            blockCrewmateExecuted = true;
-        } else
-        {
-            blockRef4_5_5.Execute();
-        }
-
-    }*/
-
-    //seashell pick up dialogue
-    /*private void OnTriggerEnter(Collider other) //seashell
-    {
-        if (haveSeashell == false)
-        {
-            blockRef5_0.Execute();
-        }
-    }*/
 }
 
     
