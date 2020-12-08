8d09f43c19e0a0df62270a1bae9f001ed1ff4ac7
leiakc <57116108+leiakc@users.noreply.github.com>
2020-12-07 20:13:29 -0500

    Map + Map Button
    - intergrated graphic for map (UI Image)
    - UI button - when pressed, shows/hides map
    - M key - when pressed, shows/hides map

---
+++ Assets/Scripts/MapShow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapShow : MonoBehaviour
+{
+
+    public GameObject map;
+    public bool viewMap = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+     void Update()
+    {
+
+        if (Input.GetKeyUp("m"))
+        {
+            DoMap();
+        }
+
+        if (viewMap == true)
+        {
+            map.SetActive(true);
+        }
+        else if (viewMap == false)
+        {
+            map.SetActive(false);
+        }
+    }
+
+    public void DoMap()
+    {
+         viewMap = !viewMap;
+
+    }
+}
